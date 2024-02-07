1fe007dc797a071f63dabfdb9f8a0ff10b155e69
domindez <109239601+domindez@users.noreply.github.com>
2024-02-07 13:12:54 +0100

    ts

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import yahooFinance from 'yahoo-finance2';
 
+const Ticker = 'TSLA'
 
 const fetchStockData = async () => {
   try {
-    const quote = await yahooFinance.quote('AAPL');
-    console.log('AAPL -', quote.regularMarketPrice);
+    const quote = await yahooFinance.quote(Ticker);
+    console.log(`${Ticker} -`, quote.regularMarketPrice);
   } catch (error) {
     console.error('Error: ', error);
   }
